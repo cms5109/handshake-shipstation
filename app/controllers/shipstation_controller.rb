31e62891c8cbce2e83f8f50a6963edd10b332130
Conal Scanlon <conal.scanlon@handshake.com>
2017-09-25 15:29:55 -0400

    trying spree format

--- app/controllers/shipstation_controller.rb
+++ app/controllers/shipstation_controller.rb
@@ -17,7 +17,7 @@ class ShipstationController < ApplicationController
 		rescue RestClient::ExceptionWithResponse => err
 			puts err.response
 		else
-			render xml: handshake_to_shipstation(order_export)
+			render xml: spree_shipstation(order_export)
 			#json_response = JSON.parse(response.body)
 		end
 
@@ -27,7 +27,7 @@ class ShipstationController < ApplicationController
 		def handshake_to_shipstation(results)
 			@results_hash = Hash.from_xml(results)
 			#puts JSON.pretty_generate(@results_hash)
-			@xml = ::Builder::XmlMarkup.new(:indent => 3)
+			@xml = Builder::XmlMarkup.new
 
 			@xml.instruct!
 			@xml.tag!("Orders", pages: pages){
@@ -110,6 +110,72 @@ class ShipstationController < ApplicationController
 		def shipstation_date(hs_date)
 			hs_date.to_datetime.strftime("%m/%d/%Y %H:%m")
 		end
+
+		def spree_shipstation(results)
+			@results_hash = Hash.from_xml(results)
+			@xml = Builder::XmlMarkup.new
+
+			@xml.instruct!
+			@xml.tag!("Orders", pages: pages){
+				@results_hash["response"]["objects"]["object"].each do |order|
+					@xml.Order {
+						@xml.OrderID 		cdata(order["objID"])
+						@xml.OrderNumber 	cdata(order["id"])
+						@xml.OrderDate		"9/25/2017 07:00"#shipstation_date(order["ctime"]))
+						@xml.OrderStatus	cdata("paid")#cdata(order["status"]))
+						@xml.LastModified	"9/25/2017 07:00"#shipstation_date(order["mtime"]))
+						@xml.shippingMethod cdata(order["shippingMethod"])
+						@xml.PaymentMethod	cdata(order["paymentTerms"])
+						@xml.OrderTotal		order["totalAmount"]
+						@xml.TaxAmount		0.00
+						@xml.ShippingAmount	0.00
+						@xml.CustomerNotes	cdata(order["notes"])
+						@xml.sourceType 	cdata(order["sourceType"])
+						@xml.Customer {
+							@xml.CustomerCode 	cdata(order["customer"]["id"])
+							@xml.BillTo {
+								@xml.Name 		cdata(order["customer"]["contact"])
+								@xml.Company 	cdata(order["customer"]["name"])
+								@xml.Phone 		cdata(order["customer"]["billTo"]["phone"])
+								@xml.Email 		cdata(order["customer"]["email"])
+
+							}
+							@xml.ShipTo {
+								@xml.Name 			cdata(order["customer"]["contact"])
+								@xml.Company 		cdata(order["customer"]["name"])
+								@xml.Address1 		cdata(order["shipTo"]["street"])
+								@xml.Address2 		cdata(order["shipTo"]["street2"])
+								@xml.City 			cdata(order["shipTo"]["city"])
+								@xml.State 			cdata("NY") #cdata(order["shipTo"]["state"])
+								@xml.PostalCode		cdata(order["shipTo"]["postcode"])
+								@xml.Country 		"US"
+								@xml.Phone 			cdata(order["shipTo"]["phone"])
+							}
+						} 
+						@xml.Items {
+							if order["lines"]
+								lines_object = order["lines"]["object"]
+								lines = lines_object.is_a?(Hash) ? [lines_object] : lines_object
+								lines.each do |line|
+									@xml.Item {
+										@xml.LineItemID 	cdata(line["objID"])
+										@xml.SKU 			cdata(line["sku"])
+										@xml.Name 			cdata(line["description"])
+										# @xml.ImageUrl
+										@xml.Weight			0.00
+										@xml.WeightUnits	"Pounds"
+										@xml.Quantity 		line["qty"]
+										@xml.UnitPrice 		line["unitPrice"]
+										# @xml.Location
+										@xml.Adjustment		true
+									}
+								end
+							end
+						}
+					}
+				end
+			}
+		end	
 
 
 end
