ebf09c03b9a783faacc147548c3307fd46dbc0ac
Conal Scanlon <conal.scanlon@handshake.com>
2017-09-27 13:19:23 -0400

    handling 1 and 0 order cases

--- app/controllers/shipstation_controller.rb
+++ app/controllers/shipstation_controller.rb
@@ -10,7 +10,7 @@ class ShipstationController < ApplicationController
 		uri_date = handshake_datetime(start_date)
 
 
-		request_uri = "https://#{api_key}:#{password}@app.handshake.com/api/v3/orders?format=xml&limit=50&offset=#{offset}&mtime__gte=#{uri_date}"
+		request_uri = "https://#{api_key}:#{password}@app.handshake.com/api/v3/orders?format=xml&limit=50&offset=#{offset}&ctime__gte=#{uri_date}"
 		puts request_uri
 
 		
@@ -59,64 +59,70 @@ class ShipstationController < ApplicationController
 
 			@xml.instruct!
 			@xml.tag!("Orders", pages: pages){
-				@results_hash["response"]["objects"]["object"].each do |order|
-					#Syntax for only single orders being returned
-					order = order.is_a?(Hash) ? [order] : order
-					@xml.tag!("Order"){
-						@xml.OrderID			{ @xml.cdata!(order["id"].to_s) }
-						@xml.OrderNumber		{ @xml.cdata!(order["objID"].to_s) }
-						@xml.OrderDate			shipstation_date(order["ctime"])
-						@xml.OrderStatus		{ @xml.cdata!(order["status"]) }
-						@xml.LastModified 		shipstation_date(order["mtime"])
-						@xml.ShippingMethod 	{ @xml.cdata!(order["shippingMethod"].to_s) }
-						@xml.PaymentMethod		{ @xml.cdata!(order["paymentTerms"].to_s) }
-						@xml.OrderTotal 		order["totalAmount"]
-						@xml.TaxAmount 			0.00
-						@xml.ShippingAmount 	0.00
-						@xml.CustomerNotes 		{ @xml.cdata!(order["notes"].to_s) }
-						@xml.Source 			{ @xml.cdata!(order["sourceType"].to_s) }
-						@xml.Customer {
-							@xml.CustomerCode { @xml.cdata!(order["customer"]["id"].to_s) }
-							@xml.BillTo {
-								@xml.Name 		{ @xml.cdata!(order["customer"]["contact"].to_s) }
-								@xml.Company 	{ @xml.cdata!(order["customer"]["name"].to_s) }
-								@xml.Phone 		{ @xml.cdata!(order["customer"]["billTo"]["phone"].to_s) }
-								@xml.Email 		{ @xml.cdata!(order["customer"]["email"].to_s) }
+				#Syntax for only single orders being returned
+				if @results_hash["response"]["objects"].nil?
+					{}
+				else
+					orders = @results_hash["response"]["objects"]["object"]
+					orders = orders.is_a?(Hash) ? [orders] : orders
 
-							}
-							@xml.ShipTo {
-								@xml.Name 		{ @xml.cdata!(order["customer"]["contact"].to_s) }
-								@xml.Company 	{ @xml.cdata!(order["customer"]["name"].to_s) }
-								@xml.Address1 	{ @xml.cdata!(order["shipTo"]["street"].to_s) }
-								@xml.Address2 	{ @xml.cdata!(order["shipTo"]["street2"].to_s) }
-								@xml.City 		{ @xml.cdata!(order["shipTo"]["city"].to_s) }
-								@xml.State 		{ @xml.cdata!(order["shipTo"]["state"].to_s) }
-								@xml.PostalCode	{ @xml.cdata!(order["shipTo"]["postcode"].to_s) }
-								@xml.Country 	{ @xml.cdata!(I18nData.country_code(order["shipTo"]["country"].to_s) || "US") }
-								@xml.Phone 		{ @xml.cdata!(order["shipTo"]["phone"].to_s) }
-							}
-						} 
-						@xml.Items {
-							if order["lines"]
-								lines_object = order["lines"]["object"]
-								lines = lines_object.is_a?(Hash) ? [lines_object] : lines_object
-								lines.each do |line|
-									@xml.Item {
-										@xml.LineItemID 	{ @xml.cdata!(line["objID"].to_s) }
-										@xml.SKU 			{ @xml.cdata!(line["sku"].to_s) }
-										@xml.Name 			{ @xml.cdata!(line["description"].to_s) }
-										@xml.ImageUrl
-										@xml.Weight 		0.00
-										@xml.WeightUnits
-										@xml.Quantity 		line["qty"]
-										@xml.UnitPrice 		line["unitPrice"]
-										@xml.Location
-										@xml.Adjustment		false
-									}
+					orders.each do |order|
+						@xml.tag!("Order"){
+							@xml.OrderID			{ @xml.cdata!(order["id"].to_s) }
+							@xml.OrderNumber		{ @xml.cdata!(order["objID"].to_s) }
+							@xml.OrderDate			shipstation_date(order["ctime"])
+							@xml.OrderStatus		{ @xml.cdata!(order["status"]) }
+							@xml.LastModified 		shipstation_date(order["mtime"])
+							@xml.ShippingMethod 	{ @xml.cdata!(order["shippingMethod"].to_s) }
+							@xml.PaymentMethod		{ @xml.cdata!(order["paymentTerms"].to_s) }
+							@xml.OrderTotal 		order["totalAmount"]
+							@xml.TaxAmount 			0.00
+							@xml.ShippingAmount 	0.00
+							@xml.CustomerNotes 		{ @xml.cdata!(order["notes"].to_s) }
+							@xml.Source 			{ @xml.cdata!(order["sourceType"].to_s) }
+							@xml.Customer {
+								@xml.CustomerCode { @xml.cdata!(order["customer"]["id"].to_s) }
+								@xml.BillTo {
+									@xml.Name 		{ @xml.cdata!(order["customer"]["contact"].to_s) }
+									@xml.Company 	{ @xml.cdata!(order["customer"]["name"].to_s) }
+									@xml.Phone 		{ @xml.cdata!(order["customer"]["billTo"]["phone"].to_s) }
+									@xml.Email 		{ @xml.cdata!(order["customer"]["email"].to_s) }
+
+								}
+								@xml.ShipTo {
+									@xml.Name 		{ @xml.cdata!(order["customer"]["contact"].to_s) }
+									@xml.Company 	{ @xml.cdata!(order["customer"]["name"].to_s) }
+									@xml.Address1 	{ @xml.cdata!(order["shipTo"]["street"].to_s) }
+									@xml.Address2 	{ @xml.cdata!(order["shipTo"]["street2"].to_s) }
+									@xml.City 		{ @xml.cdata!(order["shipTo"]["city"].to_s) }
+									@xml.State 		{ @xml.cdata!(order["shipTo"]["state"].to_s) }
+									@xml.PostalCode	{ @xml.cdata!(order["shipTo"]["postcode"].to_s) }
+									@xml.Country 	{ @xml.cdata!(I18nData.country_code(order["shipTo"]["country"].to_s) || "US") }
+									@xml.Phone 		{ @xml.cdata!(order["shipTo"]["phone"].to_s) }
+								}
+							} 
+							@xml.Items {
+								if order["lines"]
+									lines_object = order["lines"]["object"]
+									lines = lines_object.is_a?(Hash) ? [lines_object] : lines_object
+									lines.each do |line|
+										@xml.Item {
+											@xml.LineItemID 	{ @xml.cdata!(line["objID"].to_s) }
+											@xml.SKU 			{ @xml.cdata!(line["sku"].to_s) }
+											@xml.Name 			{ @xml.cdata!(line["description"].to_s) }
+											@xml.ImageUrl
+											@xml.Weight 		0.00
+											@xml.WeightUnits
+											@xml.Quantity 		line["qty"]
+											@xml.UnitPrice 		line["unitPrice"]
+											@xml.Location
+											@xml.Adjustment		false
+										}
+									end
 								end
-							end
+							}
 						}
-					}
+					end
 				end
 			}
 
