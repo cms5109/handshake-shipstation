3892c3a15bcbc58481cdf0bd257615447c892a0c
Conal Scanlon <conal.scanlon@handshake.com>
2017-09-27 13:00:51 -0400

    using mtime rather than ctime

--- app/controllers/shipstation_controller.rb
+++ app/controllers/shipstation_controller.rb
@@ -10,7 +10,7 @@ class ShipstationController < ApplicationController
 		uri_date = handshake_datetime(start_date)
 
 
-		request_uri = "https://#{api_key}:#{password}@app.handshake.com/api/v3/orders?format=xml&limit=50&offset=#{offset}&ctime__gte=#{uri_date}"
+		request_uri = "https://#{api_key}:#{password}@app.handshake.com/api/v3/orders?format=xml&limit=50&offset=#{offset}&mtime__gte=#{uri_date}"
 		puts request_uri
 
 		
