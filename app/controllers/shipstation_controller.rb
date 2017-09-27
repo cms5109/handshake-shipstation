62f15831cd3853f78c6bdbefaff18477a9093bd4
Conal Scanlon <conal.scanlon@handshake.com>
2017-09-26 21:30:08 -0400

    shipnotify

--- app/controllers/shipstation_controller.rb
+++ app/controllers/shipstation_controller.rb
@@ -7,7 +7,7 @@ class ShipstationController < ApplicationController
 		action = params['action']
 		offset = page_to_offset(params['page']) if params['page']
 		start_date = params['start_date']
-		uri_date = handshake_date(start_date)
+		uri_date = handshake_datetime(start_date)
 
 
 		request_uri = "https://#{api_key}:#{password}@app.handshake.com/api/v3/orders?format=xml&limit=50&offset=#{offset}&ctime__gte=#{uri_date}"
@@ -27,13 +27,34 @@ class ShipstationController < ApplicationController
 	end
 
 	def shipnotify
-		puts request.body.read
+		options = Hash.new
+		options['api_key'] = params['SS-UserName']
+		options['password'] = params['SS-Password']
+		options['action'] = params['action']
+		options['order_number'] = params['order_number']
+		options['carrier'] = params['carrier']
+		options['tracking_number'] = params['tracking_number']
+
+		request_uri = "https://#{options['api_key']}:#{options['password']}@app.handshake.com/api/latest/shipments"
+			
+
+		shipping_notice = request.body.read
+		handshake_shipment = shipstation_to_handshake(shipping_notice, options)
+
+		begin
+			RestClient.post(request_uri, handshake_shipment, {"Content-type" => "application/json"})
+		rescue RestClient::ExceptionWithResponse => err
+			render Hash.from_xml(err.response.body), status: err.response.code
+		else
+			render :nothing, status: :created
+		end
+		
 	end
 
 	private
 		def handshake_to_shipstation(results)
 			@results_hash = Hash.from_xml(results)
-			#puts JSON.pretty_generate(@results_hash)
+			puts JSON.pretty_generate(@results_hash)
 			@xml = Builder::XmlMarkup.new
 
 			@xml.instruct!
@@ -42,9 +63,9 @@ class ShipstationController < ApplicationController
 					@xml.tag!("Order"){
 						@xml.OrderID			{ @xml.cdata!(order["id"].to_s) }
 						@xml.OrderNumber		{ @xml.cdata!(order["objID"].to_s) }
-						@xml.OrderDate			"9/25/2017 07:00"#shipstation_date(order["ctime"]))
+						@xml.OrderDate			shipstation_date(order["ctime"])
 						@xml.OrderStatus		{ @xml.cdata!(order["status"]) }
-						@xml.LastModified 		"9/25/2017 07:00"#shipstation_date(order["mtime"]))
+						@xml.LastModified 		shipstation_date(order["mtime"])
 						@xml.ShippingMethod 	{ @xml.cdata!(order["shippingMethod"].to_s) }
 						@xml.PaymentMethod		{ @xml.cdata!(order["paymentTerms"].to_s) }
 						@xml.OrderTotal 		order["totalAmount"]
@@ -113,12 +134,54 @@ class ShipstationController < ApplicationController
 			hs_date.to_datetime.strftime("%m/%d/%Y %H:%m")
 		end
 
-		def handshake_date(ss_date)
+		def handshake_datetime(ss_date)
 			ss_date.gsub!("%2f", "/")
 			ss_date.gsub!("%3a", ":")
 			start_date = DateTime.strptime(ss_date, "%m/%d/%Y %H:%M")
 			start_date.to_datetime.strftime("%FT%T%:z")
 		end
+
+		def handshake_date(short_date)
+			date = DateTime.strptime(short_date, "%m/%d/%Y")
+			date.strftime("%FT%T%:z")
+		end
+
+		def shipstation_to_handshake(request, options={})
+			shipping_body = handshake_shipping_body(request)
+		end
+
+		def handshake_shipping_body(request)
+			shipping_hash = Hash.from_xml(request)
+			shipstation_shipment = shipping_hash["ShipNotice"]
+			handshake_shipment = Hash.new
+
+			handshake_shipment["service_level"] = shipstation_shipment["Service"]
+			handshake_shipment["order"] = order_uri(shipstation_shipment["OrderID"])
+			handshake_shipment["tracking_number"] = shipstation_shipment["TrackingNumber"]
+			handshake_shipment["notes"] = shipstation_shipment["InternalNotes"]
+			handshake_shipment["sent_to"] = sent_to(shipstation_shipment["Recipient"])
+			handshake_shipment["carrier"] = shipstation_shipment["Carrier"]
+			handshake_shipment["sent_at"] = handshake_date(shipstation_shipment["ShipDate"])
+
+			handshake_shipment.to_json
+		end
+
+		def order_uri(order_id)
+			"/api/v3/orders/#{order_id}"
+		end
+
+		def sent_to(address_hash)
+			address_string = ''
+			address_string += address_hash["Name"].to_s if address_hash["Name"]
+			address_string += ', ' + address_hash["Company"].to_s if address_hash["Company"]
+			address_string += ', ' + address_hash["Address1"].to_s if address_hash["Address1"]
+			address_string += ', ' + address_hash["Address2"].to_s if address_hash["Address2"]
+			address_string += ', ' + address_hash["City"].to_s if address_hash["City"]
+			address_string += ', ' + address_hash["State"].to_s if address_hash["State"]
+			address_string += ', ' + address_hash["PostalCode"].to_s if address_hash["PostalCode"]
+			address_string += ', ' + address_hash["Country"].to_s if address_hash["Country"]
+		end
+
 
 
 end
